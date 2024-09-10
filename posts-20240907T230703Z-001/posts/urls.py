2b10780b4fd6b622b10d8a30b9588f2eadd446b9
Mahmoud Abdelkareem Mansour Morsy <Mahmoudabdelkareem.info@gmail.com>
2024-09-11 00:36:21 +0300

    Update urls.py

--- posts-20240907T230703Z-001/posts/urls.py
+++ posts-20240907T230703Z-001/posts/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from .views import PostListCreateView, CommentListCreateView
 
 urlpatterns = [
-    path('', PostListCreateView.as_view(), name='post-list-create'),
-    path('comments/', CommentListCreateView.as_view(), name='comment-list-create'),
+    #path('', PostListCreateView.as_view(), name='post-list-create'),
+    #path('comments/', CommentListCreateView.as_view(), name='comment-list-create'),
+    path('', viwes.login, name = 'login')
+    path('postdetails', viwes.post_details, name = 'post_details')
+    path('postlist', viwes.post_list, name = 'post_list')
+    path('signup', viwes.signup, name = 'signup')
+    path('profileupdate', viwes.profile_update, name = 'profile_update')
 ]
